f1d2d427eded2d6174b615623b8533cafd6827aa
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-22 01:11:17 +0530

    adding diff.java file

--- sum.java
+++ sum.java
@@ -1,5 +1,9 @@
 public class sum{
     public static void main(String[] args) {
         System.out.println("hello world");
+        int a = 5;
+        int b = 9;
+        System.out.println(a + b);
+
     }
 }
